716c1a6665516fa244630780635b108fde10f4d8
TSweb <raymond2belinga@gmail.com>
2025-08-13 14:47:23 +0100

    Configuration de supabase avec prisma et mise en place du workflow CI.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // rend le module accessible partout
+      envFilePath: '.env', // chemin personnalisé si nécessaire
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
